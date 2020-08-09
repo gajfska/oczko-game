994120618a52d6f9a7c313b9072edbe23fcfb1ff
Werka <weronika.gajska@gmail.com>
2020-08-09 18:56:39 +0200

    adding folder structure

--- oczko-game/src/app/app.module.ts
+++ oczko-game/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { PlayerComponent } from './game/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    MainScreenComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
